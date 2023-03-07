f08b8b27f704db6f9ce99ba9ecfa589a3dff0b2b
sauhardLohan <57857933+sauhardLohan@users.noreply.github.com>
2023-03-07 09:50:52 +0530

    timer error resolved

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
     const sec=document.getElementById("sec");
     const hider=document.getElementById("hider-rotater");
     const filler=document.getElementById("filler-rotater");
+    const start=document.getElementById("start");
     hider.style.animationPlayState="paused";
     filler.style.animationPlayState="paused";
     let id=null;
@@ -12,6 +13,7 @@
 
     const secTimer=()=>{
         second++;
+        console.log(second);
         if(second==60)
         {
             second=0;
@@ -39,11 +41,11 @@
     }
 
     const startTimer=()=>{
-        id=setInterval(secTimer,1000);
         hider.classList.add("hider-animation");
         filler.classList.add("filler-animation");
         hider.style.animationPlayState="running";
         filler.style.animationPlayState="running";
+        id=setInterval(secTimer,1000);
     }
 
     const stopTimer=()=>{
@@ -67,12 +69,16 @@
         if(target.id=='start')
         {
             startTimer();
+            console.log(target);
+            target.disabled=true;
         }
         else if(target.id=='stop')
         {
+            start.disabled=false;
             stopTimer();
         }
         else if(target.id=='reset'){
+            start.disabled=false;
             stopTimer();
             resetTimer();
         }
